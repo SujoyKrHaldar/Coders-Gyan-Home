7c857dc4951233b6db5f2286648de40fd36b715c
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2025-02-06 01:43:58 +0530

    Add Animations

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,41 +1,80 @@
+import { motion } from "framer-motion";
+
 function Navbar() {
+  const navbarVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+        ease: "easeOut",
+        staggerChildren: 0.2,
+      },
+    },
+  };
   return (
-    <header className="z-100 fixed inset-0 w-full mx-auto px-40 h-fit flex items-center justify-between bg-[#191919c3] backdrop-blur-xl border-b border-dashed border-[#4a4a4a]">
-      <div className="flex items-center gap-8">
-        <div className="flex items-center gap-4">
-          <div className="w-10 h-10 p-1 bg-white rounded-full overflow-hidden">
+    <motion.header
+      initial="hidden"
+      animate="visible"
+      variants={navbarVariants}
+      className="z-100 fixed inset-0 w-full mx-auto px-40 h-fit flex items-center justify-between bg-[#191919c3] backdrop-blur-xl border-b border-dashed border-[#4a4a4a]"
+    >
+      <motion.div className="flex items-center gap-8" variants={navbarVariants}>
+        <motion.div
+          className="flex items-center gap-4"
+          variants={navbarVariants}
+        >
+          <motion.div
+            className="w-10 h-10 p-1 bg-white rounded-full overflow-hidden"
+            variants={navbarVariants}
+          >
             <img src="https://custom-webpage-static-assets-production.fermion.app/6654af5442cc797654980578-custom-zip-pages//_next/static/media/cglogo.b9879683.png" />
-          </div>
-          <p className="text-xl font-bold">Coder&apos;s Gyan</p>
-        </div>
-
-        <div className="bg-[#3e3e3e] w-[1px] h-[30px]"></div>
-
-        <nav className="flex items-center justify-between gap-10 mt-1">
-          <p className="uppercase text-sm font-medium text-[#f7d260] border-b border-[#f7d260] py-8 tracking-widest">
-            Home
-          </p>
-          <p className="uppercase text-sm tracking-widest font-medium">
-            Courses
-          </p>
-          <p className="uppercase text-sm tracking-widest font-medium">
-            Interview
-          </p>
-          <p className="uppercase text-sm tracking-widest font-medium">
-            Community
-          </p>
-        </nav>
-      </div>
-
-      <div className="flex items-center gap-4 justify-center">
-        <button className="py-3 px-6 uppercase font-medium rounded-lg text-sm tracking-widest">
+          </motion.div>
+          <motion.p className="text-xl font-bold" variants={navbarVariants}>
+            Coder&apos;s Gyan
+          </motion.p>
+        </motion.div>
+        <motion.div
+          className="bg-[#3e3e3e] w-[1px] h-[30px]"
+          variants={navbarVariants}
+        ></motion.div>
+        <motion.nav
+          className="flex items-center justify-between gap-10 mt-1"
+          variants={navbarVariants}
+        >
+          {["Home", "Courses", "Interview", "Community"].map((item, index) => (
+            <motion.p
+              key={index}
+              className={`uppercase text-sm tracking-widest font-medium ${
+                item === "Home"
+                  ? "text-[#f7d260] border-b border-[#f7d260] py-8"
+                  : ""
+              }`}
+              variants={navbarVariants}
+            >
+              {item}
+            </motion.p>
+          ))}
+        </motion.nav>
+      </motion.div>
+      <motion.div
+        className="flex items-center gap-4 justify-center"
+        variants={navbarVariants}
+      >
+        <motion.button
+          className="py-3 px-6 uppercase font-medium rounded-lg text-sm tracking-widest"
+          variants={navbarVariants}
+        >
           Login
-        </button>
-        <button className="text-white bg-[#353535] py-4 px-6 uppercase font-medium rounded-lg text-sm tracking-widest">
+        </motion.button>
+        <motion.button
+          className="text-white bg-[#353535] py-4 px-6 uppercase font-medium rounded-lg text-sm tracking-widest"
+          variants={navbarVariants}
+        >
           Book 1:1 Call
-        </button>
-      </div>
-    </header>
+        </motion.button>
+      </motion.div>
+    </motion.header>
   );
 }
 
--- src/components/pages/home/HeroSection.jsx
+++ src/components/pages/home/HeroSection.jsx
@@ -1,55 +1,120 @@
+import { motion } from "framer-motion";
+
 function HeroSection() {
+  const textVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: (i) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 1.2 + i * 0.2,
+        duration: 0.6,
+        ease: "easeOut",
+      },
+    }),
+  };
+
   return (
-    <section className="relative pt-48">
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, ease: "easeOut", delay: 1 }}
+      className="relative pt-48"
+    >
       <div className="text-center max-w-5xl mx-auto space-y-4">
-        <a
+        <motion.a
+          custom={0}
+          variants={textVariants}
+          initial="hidden"
+          animate="visible"
           href="#"
           className="p-1 pl-0 font-medium pr-6 rounded-full bg-[#f7d260] text-black text-[0.8rem]"
         >
           <span className="p-1 px-3 rounded-full bg-white mr-2">New</span>
           Fullstack Engineering Course just launched
-        </a>
+        </motion.a>
 
-        <h1 className="text-[4.5rem]/[5rem]  font-bold relative">
+        <motion.h1
+          custom={1}
+          variants={textVariants}
+          initial="hidden"
+          animate="visible"
+          className="text-[4.5rem]/[5rem] font-bold relative"
+        >
           I help you to become a{" "}
           <span className="inline-block text-[#f7d260]">
             Fullstack Engineer
           </span>
           .
-          <span className="absolute right-40 bottom-[2.5rem] rotate-[-4deg] text-xl py-2 pb-3 px-5 bg-green-700 rounded-lg tracking-normal ">
+          <motion.span
+            custom={1}
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+            className="absolute right-40 bottom-[2.5rem] rotate-[-4deg] text-xl py-2 pb-3 px-5 bg-green-700 rounded-lg tracking-normal "
+          >
             & Get Hired!
-          </span>
-        </h1>
+          </motion.span>
+        </motion.h1>
 
-        <p className="text-[1.2rem]/[2rem] max-w-lg mx-auto">
+        <motion.p
+          custom={2}
+          variants={textVariants}
+          initial="hidden"
+          animate="visible"
+          className="text-[1.2rem]/[2rem] max-w-lg mx-auto"
+        >
           Guiding future developers with simple lessons, practical tools, and
           hands-on projects to learn coding.
-        </p>
+        </motion.p>
 
         <div className="flex items-center gap-4 justify-center pt-4">
-          <button className="text-black bg-[#f7d260] py-4 px-6 uppercase font-semibold rounded-lg text-sm tracking-widest">
+          <motion.button
+            custom={3}
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+            className="text-black bg-[#f7d260] py-4 px-6 uppercase font-semibold rounded-lg text-sm tracking-widest"
+          >
             Get Started -{" "}
-            <span className="italic normal-case font-normal">Its free</span>{" "}
-          </button>
+            <span className="italic normal-case font-normal">
+              It&apos;s free
+            </span>{" "}
+          </motion.button>
 
-          <button className="uppercase font-medium text-sm tracking-widest py-4 px-6">
+          <motion.button
+            custom={4}
+            variants={textVariants}
+            initial="hidden"
+            animate="visible"
+            className="uppercase font-medium text-sm tracking-widest py-4 px-6"
+          >
             Explore Courses
-          </button>
+          </motion.button>
         </div>
       </div>
 
-      <div className=" absolute inset-0 top-1/4 -z-10 w-[800px] h-[500px] rounded-full mx-auto bg-[#7c5cfc1d] blur-3xl "></div>
+      <div className="absolute inset-0 top-1/4 -z-10 w-[800px] h-[500px] rounded-full mx-auto bg-[#7c5cfc1d] blur-3xl"></div>
 
-      <div className="w-[600px] mx-auto relative -z-10">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 1,
+          delay: 3,
+          ease: "easeOut",
+        }}
+        className="w-[600px] mx-auto relative -z-10"
+      >
         <img
           src="/profile-pic.png"
           className="w-full h-full object-cover relative contrast-120"
         />
-        <div className="absolute inset-0 top-1/4 -z-10 mx-auto w-[200px] h-[400px] rounded-full bg-[#000000b6] blur-3xl "></div>
-        <div className="absolute -bottom-20 left-1/4 -z-20 w-[500px] h-[500px] rounded-full bg-[#dca63b5b] blur-3xl "></div>
-        <div className="absolute bottom-0 right-1/4 -z-20 w-[500px] h-[500px] rounded-full bg-[#1ed83436] blur-3xl "></div>
-      </div>
-    </section>
+        <div className="absolute inset-0 top-1/4 -z-10 mx-auto w-[200px] h-[400px] rounded-full bg-[#000000b6] blur-3xl"></div>
+        <div className="absolute -bottom-20 left-1/4 -z-20 w-[500px] h-[500px] rounded-full bg-[#dca63b5b] blur-3xl"></div>
+        <div className="absolute bottom-0 right-1/4 -z-20 w-[500px] h-[500px] rounded-full bg-[#1ed83436] blur-3xl"></div>
+      </motion.div>
+    </motion.section>
   );
 }
 
